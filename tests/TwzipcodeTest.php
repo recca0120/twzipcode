30df1be3cb5e455867ecd32bb55e6714ea391b0c
Recca Tsai <recca0120.gmail.com>
2016-06-06 02:59:04 +0800

    normailize street

--- tests/TwzipcodeTest.php
+++ tests/TwzipcodeTest.php
@@ -12,22 +12,22 @@ class TwzipcodeTest extends PHPUnit_Framework_TestCase
 
     public function test_zipcode()
     {
-        $twzipcode = new Twzipcode('北 縣　萬里鄉中正路100號');
+        $twzipcode = new Twzipcode('北 縣　萬里鄉龜港村中正路100號');
         $this->assertSame($twzipcode->getZipcode(), '207');
         $this->assertSame($twzipcode->getCounty(), '新北市');
         $this->assertSame($twzipcode->getDistrict(), '萬里區');
-        $this->assertSame($twzipcode->getAddress(), '新北市萬里區中正路100號');
-        $this->assertSame($twzipcode->getShortAddress(), '中正路100號');
+        $this->assertSame($twzipcode->getAddress(), '新北市萬里區龜港村中正路100號');
+        $this->assertSame($twzipcode->getShortAddress(), '龜港村中正路100號');
     }
 
     public function test_taichung()
     {
-        $twzipcode = new Twzipcode('中縣　大里 市中正路100號');
+        $twzipcode = new Twzipcode('中縣　大里 市龜港村中正路100號');
         $this->assertSame($twzipcode->getZipcode(), '412');
         $this->assertSame($twzipcode->getCounty(), '台中市');
         $this->assertSame($twzipcode->getDistrict(), '大里區');
-        $this->assertSame($twzipcode->getAddress(), '台中市大里區中正路100號');
-        $this->assertSame($twzipcode->getShortAddress(), '中正路100號');
+        $this->assertSame($twzipcode->getAddress(), '台中市大里區龜港里中正路100號');
+        $this->assertSame($twzipcode->getShortAddress(), '龜港里中正路100號');
     }
 
     public function test_taitung()
