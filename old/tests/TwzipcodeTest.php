4abd0c0e60451629e87fe454beef1b9387f73673
Recca Tsai <recca0120.gmail.com>
2017-02-28 01:29:55 +0800

    add tricky test

---
+++ old/tests/TwzipcodeTest.php
@@ -0,0 +1,88 @@
+<?php
+
+use Mockery as m;
+use Recca0120\Twzipcode\Twzipcode;
+
+class TwzipcodeTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function test_functions()
+    {
+        $twzipcode = new Twzipcode('北 縣　萬里鄉龜港村中正路100號');
+        $twzipcode->toJson(true);
+        $twzipcode->__toString();
+        $this->assertNull($twzipcode->getAttribute('abc'));
+        $twzipcode->a();
+    }
+
+    public function test_zipcode()
+    {
+        $twzipcode = new Twzipcode('北 縣　萬里鄉龜港村中正路100號');
+        $this->assertSame($twzipcode->ge    tZipcode(), '207');
+        $this->assertSame($twzipcode->getCounty(), '新北市');
+        $this->assertSame($twzipcode->getDistrict(), '萬里區');
+        $this->assertSame($twzipcode->getAddress(), '新北市萬里區龜港村中正路100號');
+        $this->assertSame($twzipcode->getShortAddress(), '龜港村中正路100號');
+
+        $this->assertSame($twzipcode->getZipcode(true), '２０７');
+        $this->assertSame($twzipcode->getCounty(true), '新北市');
+        $this->assertSame($twzipcode->getDistrict(true), '萬里區');
+        $this->assertSame($twzipcode->getAddress(true), '新北市萬里區龜港村中正路１００號');
+        $this->assertSame($twzipcode->getShortAddress(true), '龜港村中正路１００號');
+    }
+
+    public function test_taichung()
+    {
+        $twzipcode = new Twzipcode('中縣　大里 市龜港村中正路100號');
+        $this->assertSame($twzipcode->getZipcode(), '412');
+        $this->assertSame($twzipcode->getCounty(), '臺中市');
+        $this->assertSame($twzipcode->getDistrict(), '大里區');
+        $this->assertSame($twzipcode->getAddress(), '臺中市大里區龜港里中正路100號');
+        $this->assertSame($twzipcode->getShortAddress(), '龜港里中正路100號');
+
+        $this->assertSame($twzipcode->getZipcode(true), '４１２');
+        $this->assertSame($twzipcode->getCounty(true), '臺中市');
+        $this->assertSame($twzipcode->getDistrict(true), '大里區');
+        $this->assertSame($twzipcode->getAddress(true), '臺中市大里區龜港里中正路１００號');
+        $this->assertSame($twzipcode->getShortAddress(true), '龜港里中正路１００號');
+    }
+
+    public function test_taitung()
+    {
+        $twzipcode = new Twzipcode('臺東縣　臺東 市中正路100號');
+        $this->assertSame($twzipcode->getZipcode(), '950');
+        $this->assertSame($twzipcode->getCounty(), '臺東縣');
+        $this->assertSame($twzipcode->getDistrict(), '臺東市');
+        $this->assertSame($twzipcode->getAddress(), '臺東縣臺東市中正路100號');
+        $this->assertSame($twzipcode->getShortAddress(), '中正路100號');
+
+        $this->assertSame($twzipcode->getZipcode(true), '９５０');
+        $this->assertSame($twzipcode->getCounty(true), '臺東縣');
+        $this->assertSame($twzipcode->getDistrict(true), '臺東市');
+        $this->assertSame($twzipcode->getAddress(true), '臺東縣臺東市中正路１００號');
+        $this->assertSame($twzipcode->getShortAddress(true), '中正路１００號');
+    }
+
+    public function test_taipei_datong_district_civic_blvd()
+    {
+        $twzipcode = new Twzipcode('台北市大同區市民大道一段209號5樓');
+        $this->assertSame($twzipcode->getZipcode(), '103');
+        $this->assertSame($twzipcode->getCounty(), '臺北市');
+        $this->assertSame($twzipcode->getDistrict(), '大同區');
+        $this->assertSame($twzipcode->getAddress(), '臺北市大同區市民大道一段209號5樓');
+        $this->assertSame($twzipcode->getShortAddress(), '市民大道一段209號5樓');
+
+        $this->assertSame($twzipcode->getZipcode(true), '１０３');
+        $this->assertSame($twzipcode->getCounty(true), '臺北市');
+        $this->assertSame($twzipcode->getDistrict(true), '大同區');
+        $this->assertSame($twzipcode->getAddress(true), '臺北市大同區市民大道一段２０９號５樓');
+        $this->assertSame($twzipcode->getShortAddress(true), '市民大道一段２０９號５樓');
+    }
+}
--- tests/ZipcodeTest.php
+++ tests/ZipcodeTest.php
@@ -35,6 +35,14 @@ class ZipcodeTest extends TestCase
 26044,宜蘭縣,宜蘭市,慶和街同興巷,全
 26044,宜蘭縣,宜蘭市,慶和街慶和巷,全
 26060,宜蘭縣,壯圍鄉,環市東路１段,連 374號以下
+26341,宜蘭縣,壯圍鄉,土圍路,全
+26341,宜蘭縣,壯圍鄉,大福路,全
+26341,宜蘭縣,壯圍鄉,大福路１段,全
+26341,宜蘭縣,壯圍鄉,大福路２段,全
+26345,宜蘭縣,壯圍鄉,大福路３段,全
+26343,宜蘭縣,壯圍鄉,中央路１段,全
+26344,宜蘭縣,壯圍鄉,中央路２段,全
+26341,宜蘭縣,壯圍鄉,中央路３段,全
 41274,臺中市,大里區,塗城路,單 505號以下
 41271,臺中市,大里區,塗城路,單 507號以上
 41274,臺中市,大里區,塗城路,雙 274號以下
@@ -79,5 +87,18 @@ class ZipcodeTest extends TestCase
         $this->assertSame('中正區', $zipcode->district());
         $this->assertSame('臺北市中正區中華路1段25號', $zipcode->address());
         $this->assertSame('中華路1段25號', $zipcode->shortAddress());
+    }
+
+    public function testZipcode4()
+    {
+        $address = '宜蘭縣壯圍鄉環市東路１段374號';
+        $zipcode = Zipcode::parse($address, $this->rules ?: new Rules());
+
+        $this->assertSame('260', $zipcode->zip3());
+        $this->assertSame('26060', $zipcode->zip5());
+        $this->assertSame('宜蘭縣', $zipcode->county());
+        $this->assertSame('壯圍鄉', $zipcode->district());
+        $this->assertSame('宜蘭縣壯圍鄉環市東路1段374號', $zipcode->address());
+        $this->assertSame('環市東路1段374號', $zipcode->shortAddress());
     }
 }
